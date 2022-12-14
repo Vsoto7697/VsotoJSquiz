96af2749019b8ff29363f6971581c67bfe305de0
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-12-13 19:05:29 -0500

    worked on JS and css

---
+++ assets/questions.js
@@ -0,0 +1,113 @@
+//Create Questions
+let questions = [
+    {
+        numb: 1,
+        question: "Commonly used data types DO Not Include:",
+        answer: "alerts",
+        options: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The condition in an if / else statement is enclosed within ____.",
+        answer: "parentheses",
+        options: [
+            "quotes",
+            "curly brackets",
+            "parentheses",
+            "square brackets",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Arrays in JavaScript can be used to store _________",
+        answer: "all of the above",
+        options: [
+            "number and strings",
+            "other arrays",
+            "booleans",
+            "all of the above",
+        ]
+    },
+    {
+        numb: 4,
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answer: "quotes",
+        options: [
+            "commas",
+            "curly brackets",
+            "quotes",
+            "parentheses",
+        ]
+    },
+    {
+        numb: 5,
+        question : "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "console.log",
+        options: [
+            "JavaScript",
+            "terminal / bash",
+            "for loops",
+            "console.log",
+        ]
+    },
+    {
+        numb: 6,
+        question : "What is the type of Pop up boxes available in JavaScript?",
+        answer: "All the above",
+        options: [
+            "Alert",
+            "Confirm",
+            "Prompt",
+            "All the above",
+        ]
+    },
+    {
+        numb: 7,
+        question : "Which one is a looping structure in JavaScript?",
+        answer: "All the below",
+        options: [
+            "All the below",
+            "For",
+            "While",
+            "do-while loops",
+        ]
+    },
+    {
+        numb: 8,
+        question : "What are the two basic groups of data types in JavaScript?",
+        answer: "Primitive and reference types",
+        options: [
+            "Primitive and attribute",
+            "Primitive and reference types",
+            "Reference types and attributee",
+            "None of the above",
+        ]
+    },
+    {
+        numb: 9,
+        question : "Boolean operators that can be used in JavaScript include:",
+        answer: "All the above",
+        options: [
+            "'And' Operator &&",
+            "'Or' Operator ||",
+            "'Not' Operator !",
+            "All the above",
+        ]
+    },
+    {
+        numb: 10,
+        question : "Which one of these is not among the three different types of errors in JavaScript?",
+        answer: "Animation time errors",
+        options: [
+            "Animation time errors",
+            "Load time errors",
+            "Run time errors",
+            "Logical Errors",
+        ]
+    },
+];
